2af852235459af90989e2b6206fd66ecb0ef883b
t-sin <shinichi.tanaka45@gmail.com>
2018-10-02 22:54:53 +0900

    Forth but can do nothing

---
+++ tf.lisp
@@ -0,0 +1,50 @@
+(defpackage #:tf
+  (:use #:cl)
+  (:export #:make-word
+           #:word-name
+           #:word-code
+
+           #:make-vm
+           #:vm-dict
+           #:vm-stack
+
+           #:parse
+           #:evaluate-atom
+           #:evaluate))
+(in-package #:tf)
+
+(defun parse (stream)
+  (let (code buf atomp)
+    (flet ((read-atom (ch)
+             (unless atomp
+               (setf atomp t)
+               (setf buf nil))
+             (push ch buf))
+           (terminate-atom ()
+             (when atomp
+               (setf atomp nil)
+               (push (intern (concatenate 'string (nreverse buf))) code))))
+      (loop
+        :for ch := (read-char stream nil :eof)
+        :until (eq ch :eof)
+        :do (case ch
+              (#\space (terminate-atom))
+              (#\newline (terminate-atom))
+              (t (read-atom ch)))
+        :finally (progn
+                   (terminate-atom)
+                   (return (nreverse code)))))))
+
+(defstruct word name code)
+(defstruct vm dict stack)
+
+(defun evaluate-atom (atom vm)
+  (let ((word (find atom (vm-dict vm) :key #'word-name)))
+    (if word
+        (funcall (word-code word) vm)
+        (push atom (vm-stack vm)))))
+
+(defun evaluate (code vm)
+  (loop
+    :for atom :in code
+    :do (evaluate-atom atom vm)))
