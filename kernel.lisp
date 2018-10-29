49f71284c66001a5ddf5fcc1e1fa4c698f0ac621
t-sin <shinichi.tanaka45@gmail.com>
2018-10-30 00:31:29 +0900

    Stacks as a closure

--- kernel.lisp
+++ kernel.lisp
@@ -13,3 +13,20 @@
 
 (defstruct vm
   ip compiling dict pstack rstack cstack)
+
+(defun make-stack (size)
+  (let ((stack (make-array size :element-type 'cell))
+        (top 0))
+    (lambda (method &optional val)
+      (case method
+        (:push (if (< size (1+ top))
+                   (values nil :exausted)
+                   (progn
+                     (setf (svref stack top) val)
+                     (incf top))))
+        (:pop (progn
+                (decf top)
+                (cond ((< top 0) (progn
+                                   (setf top 0)
+                                   (values nil :empty)))
+                      (t (values (svref stack top) t)))))))))
