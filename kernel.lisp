5ba0982f62057b66beb9c76a83535afc4a8bc0de
t-sin <shinichi.tanaka45@gmail.com>
2018-10-30 17:42:36 +0900

    Initialize VM

--- kernel.lisp
+++ kernel.lisp
@@ -9,10 +9,13 @@
   type data)
 
 (defstruct word
-  id prev name code immediate)
+  prev name code immediate)
 
 (defstruct vm
   ip compiling dict pstack rstack cstack)
+
+(defun make-word% (dict name code immediate)
+  (make-word :prev dict :name name :code code :immediate immediate))
 
 (defun make-stack (size)
   (let ((stack (make-array size :element-type 'cell))
@@ -30,3 +33,16 @@
                                    (setf top 0)
                                    (values nil :empty)))
                       (t (values (svref stack top) t)))))))))
+
+(defvar pstack-size 1000)
+(defvar rstack-size 1000)
+(defvar cstack-size 1000)
+
+(defun init-vm ()
+  (let* ((vm (make-vm :ip nil
+                      :compiling nil
+                      :dict (make-word% nil nil nil nil)
+                      :pstack (make-stack pstack-size)
+                      :rstack (make-stack rstack-size)
+                      :cstack (make-stack cstack-size))))
+    vm))
