09c21c19472f0c9a8c7552ba9d660267cd1e60d9
t-sin <shinichi.tanaka45@gmail.com>
2018-12-22 15:17:42 +0900

    Add function that read to specified charactter; to implement `WORD`

--- main.lisp
+++ main.lisp
@@ -14,12 +14,16 @@
 (defstruct vm
   program dict ip comp? dstack rstack)
 
+;;;;
+;; readers
+
 (defvar +delimiters+ '(#\space #\tab #\newline #\page))
 
-(defun skip-delimiters (stream)
+(defun skip-delimiters (stream &optional (num nil))
   (loop
     :for ch := (peek-char nil stream nil :eof)
-    :while (member ch +delimiters+)
+    :for n := 0 :then (1+ n)
+    :while (and (or (null num) (<= n num)) (member ch +delimiters+))
     :do (read-char stream)))
 
 (defun next-token (stream)
@@ -29,5 +33,18 @@
       :for ch := (peek-char nil stream nil :eof)
       :if (or (eq ch :eof) (member ch +delimiters+ :test #'char=))
       :do (return-from next-token (coerce (nreverse buf) 'string))
+      :else
+      :do (push (read-char stream) buf))))
+
+(defun read-to (ch stream)
+  (let (buf)
+    (skip-delimiters stream 1)
+    (loop
+      :for ch* := (peek-char nil stream nil :eof)
+      :if (or (eq ch* :eof) (char= ch* ch))
+      :do (progn
+            (unless (eq ch* :eof)
+              (read-char stream))
+            (return-from read-to (coerce (nreverse buf) 'string)))
       :else
       :do (push (read-char stream) buf))))
