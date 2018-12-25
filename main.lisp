cd325b2728a76fd5856f6ae0dca62e140c453abe
t-sin <shinichi.tanaka45@gmail.com>
2018-12-25 12:20:14 +0900

    Make a package for readers

--- main.lisp
+++ main.lisp
@@ -1,44 +1,12 @@
 (defpackage #:uf
   (:use #:cl)
+  (:import-from #:uf/reader
+                #:next-token
+                #:read-to)
   (:export))
 (in-package #:uf)
 
 (define-condition uf/error (simple-error) ())
-
-;;;;
-;; readers
-
-(defvar +delimiters+ '(#\space #\tab #\newline #\page))
-
-(defun skip-delimiters (stream &optional (num nil))
-  (loop
-    :for ch := (peek-char nil stream nil :eof)
-    :for n := 0 :then (1+ n)
-    :while (and (or (null num) (<= n num)) (member ch +delimiters+))
-    :do (read-char stream)))
-
-(defun next-token (stream)
-  (let (buf)
-    (skip-delimiters stream)
-    (loop
-      :for ch := (peek-char nil stream nil :eof)
-      :until (or (eq ch :eof) (member ch +delimiters+ :test #'char=))
-      :do (push (read-char stream) buf)
-      :finally (unless (null buf)
-                 (return-from next-token (coerce (nreverse buf) 'string))))))
-
-(defun read-to (ch stream)
-  (let (buf)
-    (skip-delimiters stream 1)
-    (loop
-      :for ch* := (peek-char nil stream nil :eof)
-      :if (or (eq ch* :eof) (char= ch* ch))
-      :do (progn
-            (unless (eq ch* :eof)
-              (read-char stream))
-            (return-from read-to (coerce (nreverse buf) 'string)))
-      :else
-      :do (push (read-char stream) buf))))
 
 ;;;;
 ;; stack
