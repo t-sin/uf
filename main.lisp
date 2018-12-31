98199ea6fdff0ba9683b9aa9ceee825fc38933ee
t-sin <shinichi.tanaka45@gmail.com>
2019-01-01 02:08:31 +0900

    Set debug after initializing

--- main.lisp
+++ main.lisp
@@ -4,6 +4,7 @@
                 #:uf/error)
   (:import-from #:uf/vm
                 #:vm-stream
+                #:vm-debug
                 #:vm-compbuf
                 #:make-vm*
                 #:interpret)
@@ -17,9 +18,10 @@
 ;; runtime
 
 (defun init-vm (stream &optional (debug nil) (word-list *initial-word-list*))
-  (let ((vm (make-vm* debug)))
+  (let ((vm (make-vm* nil)))
     (loop
       :for def :in (reverse word-list)
       :do (funcall def vm))
-    (setf (vm-stream vm) stream)
+    (setf (vm-stream vm) stream
+          (vm-debug vm) debug)
     vm))
