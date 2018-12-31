12fcdd3b34454b8c9652233cfc619f7e3ea58ffe
t-sin <shinichi.tanaka45@gmail.com>
2018-12-31 20:43:24 +0900

    oops

--- builtin/primitive.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage #:uf/builtin
-  (:use #:cl #:uf/reader #:uf/stack #:uf/vm)
-  (:import-from #:uf/builtin/define
-                #:defword))
-(in-package #:uf/builtin)
-;; VM
-
-;; I/O
-
