5d0f5df9ba857a398ca0c428f1453bd5e823b8ac
t-sin <shinichi.tanaka45@gmail.com>
2019-01-12 14:39:20 +0900

    Proceed in stack manupilation words

--- builtin/stack.lisp
+++ builtin/stack.lisp
@@ -6,10 +6,14 @@
 (in-package #:uf/builtin/stack)
 
 (defword ("dup" nil nil)
-  (stack-push (stack-top (vm-pstack vm)) (vm-pstack vm)))
+  (progn
+    (stack-push (stack-top (vm-pstack vm)) (vm-pstack vm))
+    (vm/next vm)))
 
 (defword ("drop" nil nil)
-  (stack-pop (vm-pstack vm)))
+  (progn
+    (stack-pop (vm-pstack vm))
+    (vm/next vm)))
 
 (defword ("swap" nil nil)
   (let* ((pstack (vm-pstack vm))
@@ -18,7 +22,8 @@
         (error "stack length is less than 2. cannot swap.")
         (let ((o1 (svref (stack-vec pstack) (- ptr 1))))
           (setf (svref (stack-vec pstack) (- ptr 1)) (svref (stack-vec pstack) (- ptr 2))
-                (svref (stack-vec pstack) (- ptr 2)) o1)))))
+                (svref (stack-vec pstack) (- ptr 2)) o1)))
+    (vm/next vm)))
 
 (defword ("over" nil nil)
   (let* ((pstack (vm-pstack vm))
@@ -26,7 +31,8 @@
     (if (< ptr 2)
         (error "stack length is less than 2. cannot over.")
         (let ((o (svref (stack-vec pstack) (- ptr 2))))
-          (stack-push o pstack)))))
+          (stack-push o pstack)))
+    (vm/next vm)))
 
 (defword ("rot" nil nil)
   (let* ((pstack (vm-pstack vm))
@@ -37,4 +43,5 @@
               (o2 (svref (stack-vec pstack) (- ptr 2))))
           (setf (svref (stack-vec pstack) (- ptr 1)) (svref (stack-vec pstack) (- ptr 3))
                 (svref (stack-vec pstack) (- ptr 2)) o1
-                (svref (stack-vec pstack) (- ptr 3)) o2)))))
+                (svref (stack-vec pstack) (- ptr 3)) o2)))
+    (vm/next vm)))
