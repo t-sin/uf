9ef0fa8e3305a91dd4539fcd820be41d9f439e68
t-sin <shinichi.tanaka45@gmail.com>
2019-01-16 17:37:07 +0900

    Fix typo

--- builtin/controll.lisp
+++ builtin/controll.lisp
@@ -1,8 +1,8 @@
 (defpackage #:uf/builtin/controll
   (:use #:cl #:uf/reader #:uf/stack #:uf/vm)
-  (:import #:uf/builtin/define
-           #:defword
-           #:exec))
+  (:import-from #:uf/builtin/define
+                #:defword
+                #:exec))
 (in-package #:uf/builtin/controll)
 
 (defword ("" nil nil)
